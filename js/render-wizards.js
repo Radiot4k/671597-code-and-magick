4d70f9852a7e82798d409eaf2ff6f55449887461
Radiot4k <radiot4k@gmail.com>
2018-07-03 16:06:07 +0300

    Сортировка похожих магов

--- js/render-wizards.js
+++ js/render-wizards.js
@@ -1,29 +1,56 @@
 'use strict';
 
 (function () {
-  var similarListElement = document.querySelector('.setup-similar-list');
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var coatColor;
+  var eyesColor;
+  var wizards = [];
 
-  var renderWizard = function (wizard) {
-    var wizardElement = similarWizardTemplate.cloneNode(true);
+  var getRank = function (wizard) {
+    var rank = 0;
 
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
 
-    return wizardElement;
+    return rank;
   };
 
-  var onSuccess = function (wizards) {
-    var fragment = document.createDocumentFragment();
-    var i = 0;
-    while (i < 4) {
-      var wizardNumber = window.util.getRandomNumber(0, wizards.length - 1);
-      fragment.appendChild(renderWizard(wizards[wizardNumber]));
-      i++;
+  var namesComparator = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
     }
-    similarListElement.appendChild(fragment);
-    window.util.setup.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var updateWizards = function () {
+    window.render(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
+  };
+
+  window.wizard.onEyesChange = window.debounce(function (color) {
+    eyesColor = color;
+    updateWizards();
+  });
+
+  window.wizard.onCoatChange = window.debounce(function (color) {
+    coatColor = color;
+    updateWizards();
+  });
+
+  var onSuccess = function (data) {
+    wizards = data;
+    window.render(wizards);
   };
 
   var onError = function (errorMessage) {
